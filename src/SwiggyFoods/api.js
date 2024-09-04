7621ada1df67408f19f1e4c3e71f98d781589529
manoj-fsd-csc <manojsundaram541@gmail.com>
2024-09-04 22:36:05 +0530

    Change API Path

--- src/SwiggyFoods/api.js
+++ src/SwiggyFoods/api.js
@@ -1,2 +1,2 @@
-// export const API_URL = "https://backend-nodejs-swiggyfoods.onrender.com/"
-export const API_URL = "http://localhost:4000";
+export const API_URL = "https://backend-nodejs-swiggyfoods.onrender.com/"
+// export const API_URL = "http://localhost:4000";
