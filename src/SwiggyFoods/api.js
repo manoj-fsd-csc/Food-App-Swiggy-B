d597c7fd1f0c03d41278ad272f0cd302bca5f54b
manoj-fsd-csc <manojsundaram541@gmail.com>
2024-09-06 18:50:28 +0530

    Change API Path into live

--- src/SwiggyFoods/api.js
+++ src/SwiggyFoods/api.js
@@ -1,6 +1,6 @@
 
 //FIRST
- export const  API_URL = "https://backend-nodejs-swiggyfoods.onrender.com/"
+ export const  API_URL = "https://backend-nodejs-swiggyfoods.onrender.com"
 
 //SECOND 
 //  export  const  API_URL = "https://food-app-backend-jsmw.onrender.com"
